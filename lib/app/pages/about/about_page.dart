0f5dc8b10948bacf34baf70ee033056166206032
Rahmad Farizan <57408301+Rahmadfarizan@users.noreply.github.com>
2024-10-26 16:34:47 +0700

    change get package to lastest version

--- lib/app/pages/about/about_page.dart
+++ lib/app/pages/about/about_page.dart
@@ -10,6 +10,9 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Text("About"),
+    );
   }
 }
