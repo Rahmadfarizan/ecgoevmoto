a5a48a872c7ddc6ce88bd9f06ff7f6f39f23e264
Rahmad Farizan <57408301+Rahmadfarizan@users.noreply.github.com>
2024-10-26 17:11:08 +0700

    Deploy Flutter Web App

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); 
   runApp(const MyApp());
 }
 
