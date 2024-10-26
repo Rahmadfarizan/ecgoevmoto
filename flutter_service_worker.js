8f20e2a7f7af354b0a737d1c43f29935b08e3b23
Rahmad Farizan <57408301+Rahmadfarizan@users.noreply.github.com>
2024-10-26 16:46:57 +0700

    Deploy Flutter Web App

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "65183a8532b4a4869fe8faac1f58c81f",
+"flutter_bootstrap.js": "6420d0c977fca79f15432dcc02a05e66",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
